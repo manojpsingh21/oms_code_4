eacbbee8d96fe97cc2ff78fba99b5ff991d4f201
Manoj Vishwakarma <Manoj Vishwakarma@adrupeeseed.com>
2024-10-25 13:19:23 +0530

    Some changes added

--- start_setup.sh
+++ start_setup.sh
@@ -1,7 +1,11 @@
-mv ./oms-src4.0-DUMMY_MASTER/* ./
-mv ./oms-src4.0-DUMMY_MASTER/.bash_profile ./oms-src4.0-DUMMY_MASTER/.bashrc ./oms-src4.0-DUMMY_MASTER/.gitignore ./
+sudo dnf install -y dos2unix
+sudo chown -R $USER:$USER /home/$USER/
+mv ~/oms-src4.0-DUMMY_MASTER/* ~/
+mv ~/oms-src4.0-DUMMY_MASTER/.bash_profile ./oms-src4.0-DUMMY_MASTER/.bashrc ./oms-src4.0-DUMMY_MASTER/.gitignore ~/
+dos2unix ~/.bashrc
 source ~/.bashrc
-chown -R $USER:$USER /home/$USER/
-sudo dnf install /home/OM-TEST123/MySQL-Cluster-*
-sudo dnf install git 
-rm -rf /home/OM-TEST123/MySQL-Cluster-*
+mkdir -p ~/Application/Objects
+#chown -R $USER:$USER /home/$USER/
+#sudo dnf install /home/OM-TEST123/MySQL-Cluster-*
+#sudo dnf install git 
+#rm -rf /home/OM-TEST123/MySQL-Cluster-*
