9b09cc20e10cb4a5e035bc57594114bc361125f5
Manoj Vishwakarma <Manoj Vishwakarma@adrupeeseed.com>
2024-10-28 17:48:38 +0530

    latest things added

--- start_setup.sh
+++ start_setup.sh
@@ -5,7 +5,23 @@ mv ~/oms-src4.0-DUMMY_MASTER/.bash_profile ./oms-src4.0-DUMMY_MASTER/.bashrc ./o
 dos2unix ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/Application/Objects
-#chown -R $USER:$USER /home/$USER/
-#sudo dnf install /home/OM-TEST123/MySQL-Cluster-*
-#sudo dnf install git 
-#rm -rf /home/OM-TEST123/MySQL-Cluster-*
+sudo dnf install $HOME/MySQL-Cluster-*
+sudo dnf install git 
+rm -rf $HOME/MySQL-Cluster-*
+
+
+
+cd /shared_migration/
+wget https://archives.boost.io/release/1.80.0/source/boost_1_80_0.tar.gz
+tar xvfz boost_1_80_0.tar.gz
+cd boost_1_80_0/
+./bootstrap.sh
+./b2 --with-serialization --with-thread --with-program_options --with-log --with-filesystem --with-chrono --with-system --with-date_time
+
+cd ~/
+wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
+tar xvfz openssl-1.1.1w.tar.gz
+cd openssl-1.1.1w/
+./config --prefix="/shared_migration/openssl_1.1.1w/"
+make -j 4
+
