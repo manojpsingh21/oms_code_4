3788fae27ae2f40ae2a9c0471ac9051a75db0cfd
Manoj Vishwakarma <Manoj Vishwakarma@adrupeeseed.com>
2024-11-02 12:55:51 +0530

    NseFOOrdSvr libnsl added

--- start_setup.sh
+++ start_setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 sudo dnf install -y wget which
 sudo dnf install -y coreutils --allowerasing
+sudo ln -s /usr/lib64/libnsl.so.1 /usr/lib64/libnsl.so
 sudo chown -R $USER:$USER /home/$USER/
 mv ~/oms-src4.0-DUMMY_MASTER/* ~/
 mv ~/oms-src4.0-DUMMY_MASTER/.bash_profile ./oms-src4.0-DUMMY_MASTER/.bashrc ./oms-src4.0-DUMMY_MASTER/.gitignore ~/
 dos2unix ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/Application/Objects
+mkdir -p ~/Application/Exec/Run
 sudo dnf install -y $HOME/MySQL-Cluster-*
 sudo dnf install -y git 
 rm -rf $HOME/MySQL-Cluster-*
@@ -26,8 +28,7 @@ tar xvfz openssl-1.1.1w.tar.gz
 cd openssl-1.1.1w/
 ./config --prefix="/shared_migration/openssl_1.1.1w/"
 make -j 4
-
-
+make install 
 sudo dnf install -y gnutls-devel libtiff libX11 giflib GConf2 libXft Xaw3d dbus-devel  dbus-glib-devel gpm-devel gtk3-devel jansson-devel libX11-devel libtiff-devel ncurses-devel webkit2gtk3
 sudo dnf groupinstall -y "Development Tools"
-
+rm -rf openssl-1.1.1w.tar.gz oms-src4.0-DUMMY_MASTER.tar.gz oms-src4.0-DUMMY_MASTER mysql_rpm.tar.gz openssl-1.1.1w wget-log
